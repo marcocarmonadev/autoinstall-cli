cb8ca7af80ded69cd5ec3611c29b1b3267649de5
Marco Carmona <marcocarmonadev@outlook.com>
2024-07-25 12:09:26 -0600

    Update url

--- source/__init__.py
+++ source/__init__.py
@@ -20,7 +20,7 @@ def callback(): ...
 def pgAdmin():
     try:
         response = requests.get(
-            "https://gist.githubusercontent.com/marcocarmonadev/313402864136e2be615665636ddc6f10/raw/9f79033c748959bd2bff475e0d136a133675817c/pgAdmin"
+            "https://raw.githubusercontent.com/marcocarmonadev/autoinstall-cli/main/scripts/pgAdmin"
         )
         raw_data = response.text
         response.raise_for_status()
