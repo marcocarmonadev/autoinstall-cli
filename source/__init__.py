6c75645e085d56ff32c84383680a1c49677c7bb0
Marco Carmona <marcocarmonadev@outlook.com>
2024-07-24 21:34:42 -0600

    All done!

---
+++ source/__init__.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import requests
+import typer
+
+cli = typer.Typer()
+
+CURRENT_PATH = Path()
+HOME = CURRENT_PATH.home()
+PGADMIN_EXEC_PATH = HOME / ".local/bin/pgAdmin"
+
+
+@cli.callback()
+def callback(): ...
+
+
+@cli.command(
+    name="pgAdmin",
+)
+def pgAdmin():
+    try:
+        response = requests.get(
+            "https://gist.githubusercontent.com/marcocarmonadev/313402864136e2be615665636ddc6f10/raw/7bbe9ae55905a6d02b7da5edbe8074840c10bd11/pgAdmin"
+        )
+        raw_data = response.text
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("Connection error.")
+    else:
+        PGADMIN_EXEC_PATH.touch(
+            exist_ok=True,
+        )
+        PGADMIN_EXEC_PATH.write_text(raw_data)
+        PGADMIN_EXEC_PATH.chmod(0o775)
+        print("Reload your shell.")
