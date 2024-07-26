3bef5df3a79c471717bc8edf6b072246d9f89d8f
Marco Carmona <marcocarmonadev@outlook.com>
2024-07-26 11:02:17 -0600

    Update __init__.py & pyproject.toml

--- source/__init__.py
+++ source/__init__.py
@@ -8,28 +8,48 @@ cli = typer.Typer()
 CURRENT_PATH = Path()
 HOME = CURRENT_PATH.home()
 PGADMIN_EXEC_PATH = HOME / ".local/bin/pgAdmin"
+UPGRADE_SYSTEM_EXEC_PATH = HOME / ".local/bin/upgrade-system"
 
 
-@cli.callback()
-def callback(): ...
+def download_raw_data(
+    script_name: str,
+):
+    try:
+        response = requests.get(
+            f"https://raw.githubusercontent.com/marcocarmonadev/autoinstall-cli/main/scripts/{script_name}"
+        )
+        raw_data = response.text
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("Connection error.")
+        raise typer.Abort()
+    else:
+        return raw_data
 
 
 @cli.command(
     name="pgAdmin",
 )
 def pgAdmin():
-    try:
-        response = requests.get(
-            "https://raw.githubusercontent.com/marcocarmonadev/autoinstall-cli/main/scripts/pgAdmin"
-        )
-        raw_data = response.text
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("Connection error.")
-    else:
-        PGADMIN_EXEC_PATH.touch(
-            exist_ok=True,
-        )
-        PGADMIN_EXEC_PATH.write_text(raw_data)
-        PGADMIN_EXEC_PATH.chmod(0o775)
-        print("Reload your shell.")
+    raw_data = download_raw_data(
+        script_name="pgAdmin",
+    )
+    PGADMIN_EXEC_PATH.touch(
+        exist_ok=True,
+    )
+    PGADMIN_EXEC_PATH.write_text(raw_data)
+    PGADMIN_EXEC_PATH.chmod(0o775)
+    print("Reload your shell.")
+
+
+@cli.command()
+def upgrade_system():
+    raw_data = download_raw_data(
+        script_name="upgrade-system",
+    )
+    UPGRADE_SYSTEM_EXEC_PATH.touch(
+        exist_ok=True,
+    )
+    UPGRADE_SYSTEM_EXEC_PATH.write_text(raw_data)
+    UPGRADE_SYSTEM_EXEC_PATH.chmod(0o775)
+    print("Reload your shell.")
